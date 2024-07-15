91ba7c115fb86e998cf703c56691450104469bcd
Verdilorn <verdilorn@gmail.com>
2024-07-15 18:46:20 +0900

    One by One

--- Script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const dropdownButton = document.querySelector('.dropdown-button');
-    const detailedStatus = document.querySelector('.detailed-status');
-
-    dropdownButton.addEventListener('click', () => {
-        detailedStatus.style.display = detailedStatus.style.display === 'block' ? 'none' : 'block';
-        dropdownButton.classList.toggle('active');
-    });
-});
