4e9a0b4accc14e650553f5622e4a23dd690b1eac
Verdilorn <verdilorn@gmail.com>
2024-07-15 18:27:09 +0900

    Sidebar

---
+++ Script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdownButton = document.querySelector('.dropdown-button');
+    const detailedStatus = document.querySelector('.detailed-status');
+
+    dropdownButton.addEventListener('click', () => {
+        detailedStatus.style.display = detailedStatus.style.display === 'block' ? 'none' : 'block';
+    });
+});
